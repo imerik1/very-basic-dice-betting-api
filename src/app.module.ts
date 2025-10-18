a2335754cc0e186e37f2e0871a35f80860f59e8a
Erik Marques <erik.marques@ekerdev.com>
2025-10-18 17:50:24 -0300

    feat(app): adjust app for feature package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,23 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { Bet } from '~/bet/entity/bet.entity';
+import { User } from '~/user/entity/user.entity';
+import { BetModule } from './bet/bet.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    SequelizeModule.forRoot({
-      dialect: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      models: [],
-    }),
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'test',
+            models: [User, Bet],
+        }),
+        BetModule,
+        UserModule,
+    ],
 })
 export class AppModule {}
