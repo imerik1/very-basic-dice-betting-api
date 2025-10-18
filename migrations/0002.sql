c088cf33d6df07852378b62ac79507164051b824
Erik Marques <erik.marques@ekerdev.com>
2025-10-18 19:29:27 -0300

    feat(migration): updating initial balance

--- migrations/0002.sql
+++ migrations/0002.sql
@@ -1,6 +1,6 @@
-INSERT INTO users(name, balance) VALUES ("john", 0);
-INSERT INTO users(name, balance) VALUES ("bob", 0);
-INSERT INTO users(name, balance) VALUES ("cristiano", 0);
-INSERT INTO users(name, balance) VALUES ("ronaldo", 0);
-INSERT INTO users(name, balance) VALUES ("taylor", 0);
-INSERT INTO users(name, balance) VALUES ("billy", 0);
+INSERT INTO users(name, balance) VALUES ("john", 400);
+INSERT INTO users(name, balance) VALUES ("bob", 200);
+INSERT INTO users(name, balance) VALUES ("cristiano", 50);
+INSERT INTO users(name, balance) VALUES ("ronaldo", 150);
+INSERT INTO users(name, balance) VALUES ("taylor", 250);
+INSERT INTO users(name, balance) VALUES ("billy", 130);
