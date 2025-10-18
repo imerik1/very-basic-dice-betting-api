613d0dda6dc3b450a1135ee5fe86ffb670c5258d
Erik Marques <erik.marques@ekerdev.com>
2025-10-18 17:51:13 -0300

    feat(user): add user module

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,38 @@
+import Decimal from 'decimal.js';
+import {
+    AutoIncrement,
+    Column,
+    DataType,
+    HasMany,
+    Model,
+    PrimaryKey,
+    Table,
+} from 'sequelize-typescript';
+import { Bet } from '~/bet/entity/bet.entity';
+import { decimalModel } from '~/shared/utils/decimal';
+
+@Table({
+    tableName: 'users',
+    createdAt: false,
+    updatedAt: false,
+    deletedAt: false,
+    version: false,
+})
+export class User extends Model {
+    @PrimaryKey
+    @AutoIncrement
+    @Column({ type: DataType.INTEGER, field: 'id' })
+    declare id: number;
+
+    @Column({ type: DataType.STRING(50), field: 'balance' })
+    declare name: string;
+
+    @Column({
+        field: 'balance',
+        ...decimalModel('', 19, 4),
+    })
+    declare balance: Decimal;
+
+    @HasMany(() => Bet)
+    declare bets: Bet[];
+}
