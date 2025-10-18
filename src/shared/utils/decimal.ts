90d13ee877b5c9f0b105e80e79bf1aae0301b13f
Erik Marques <erik.marques@ekerdev.com>
2025-10-18 17:50:52 -0300

    feat(decimal): add util function decimal model default

---
+++ src/shared/utils/decimal.ts
@@ -0,0 +1,20 @@
+import Decimal from 'decimal.js';
+import { ModelAttributeColumnOptions } from 'sequelize';
+import { DataType } from 'sequelize-typescript';
+
+export const decimalModel = (
+    columnName: string,
+    precision: number,
+    rounding: Decimal.Rounding,
+): Partial<ModelAttributeColumnOptions> => {
+    return {
+        type: DataType.DECIMAL(precision, rounding),
+        get: function () {
+            Decimal.set({ precision, rounding });
+            return new Decimal(this.getDataValue(columnName));
+        },
+        set: function (value: Decimal) {
+            this.setDataValue(columnName, value.toString());
+        },
+    };
+};
