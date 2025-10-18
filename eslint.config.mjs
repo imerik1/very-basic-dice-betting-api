58ba6b661a9ef2ba8482d2cd62ddd04715a8b998
Erik Marques <erik.marques@ekerdev.com>
2025-10-18 16:55:35 -0300

    build(lint): adjust eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,16 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
+  },
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -28,6 +34,9 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-floating-promises': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
